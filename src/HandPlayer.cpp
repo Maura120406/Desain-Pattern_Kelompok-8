4d8e2ceccab7e1c3eb587aa4255ca74c22188aaa
AisyahDwiSlametRahayu <akunwibu05@gmail.com>
2026-04-07 12:23:59 +0700

    Update HandPlayer.cpp

--- src/HandPlayer.cpp
+++ src/HandPlayer.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 #include "HandPlayer.h"
+
 void HandPlayer::playHand()
 {
-std::cout << "Player selects cards to play...\n";
+    std::cout << "Player selects cards...\n";
+
+    // sementara dummy (nanti dari generator)
+    hand = {
+        Card(10,"H"),
+        Card(11,"H"),
+        Card(12,"H"),
+        Card(13,"H"),
+        Card(14,"H")
+    };
+}
+
+std::vector<Card> HandPlayer::getHand() const {
+    return hand;
 }
