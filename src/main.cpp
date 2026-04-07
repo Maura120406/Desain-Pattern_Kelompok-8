c2d74d1f53146c87658dab5aeba138019c0a0f48
AisyahDwiSlametRahayu <akunwibu05@gmail.com>
2026-04-07 12:25:31 +0700

    Add runSession method to GameManager
    Implement runSession method in GameManager class

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
-#include "GameManager.h"
-int main()
-{
-GameManager gameManager;
-gameManager.runSession();
-return 0;
+void GameManager::runSession(){
+    std::cout << "=== Run Started ===\n";
+
+    handGenerator.generateHand();
+    handPlayer.playHand();
+  
+    std::vector<Card> hand = handPlayer.getHand();
+
+    std::string result = HandEvaluator::evaluate(hand);
+
+    std::cout << "Hand Result: " << result << "\n";
+
+    int score = scoringRule.scoreHand(result);
+
+    bool win = blindRule.checkBlind(score);
+    int reward = rewardRule.earnMoney(win, score);
+
+    std::cout << "Money gained: " << reward << "\n";
+    std::cout << "=== Run Ended ===\n";
 }
