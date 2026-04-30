81a83b1775a63635be4a0e21fccbe62d8b562708
AisyahDwiSlametRahayu <akunwibu05@gmail.com>
2026-04-30 15:00:04 +0700

    update HandG

---
+++ CheckerSystem/HandGenerator.cpp
@@ -0,0 +1,23 @@
+#include "HandGenerator.h"
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <ctime>
+
+std::vector<Card> HandGenerator::generateHand() {
+    std::vector<Card> deck;
+
+    std::vector<std::string> suits = { "H", "D", "C", "S" };
+
+    for (int rank = 2; rank <= 14; rank++) {
+        for (const auto& suit : suits) {
+            deck.push_back(Card(rank, suit));
+        }
+    }
+
+    std::shuffle(deck.begin(), deck.end(), std::default_random_engine(std::time(0)));
+  
+    std::vector<Card> hand(deck.begin(), deck.begin() + 5);
+
+    return hand;
+}
