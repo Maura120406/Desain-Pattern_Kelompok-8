f8d216a0cbb0b1a5e4b3c51f47d3e8d6037aebb0
Maura120406 <maura12046@gmail.com>
2026-06-02 23:10:56 +0700

    Update JokerManager

--- CheckerSystem/JokerManager.cpp
+++ CheckerSystem/JokerManager.cpp
@@ -3,6 +3,14 @@
 #include "JokerManager.h"
 #include "MultiplierJoker.h"
 #include "BonusJoker.h"
+#include "MultiplierJoker.h"
+#include "BonusJoker.h"
+#include "LuckyJoker.h"
+#include "MegaMultiplierJoker.h"
+#include "RichJoker.h"
+#include "DoubleJoker.h"
+#include "HybridJoker.h"
+
 
 void JokerManager::chooseJokers() {
 
@@ -29,4 +37,12 @@ void JokerManager::chooseJokers() {
 const std::vector<Joker*>&
 JokerManager::getSelectedJokers() const {
     return selectedJokers;
+
+ std::cout << "1. Multiplier Joker\n";
+std::cout << "2. Bonus Joker\n";
+std::cout << "3. Lucky Joker\n";
+std::cout << "4. Mega Multiplier Joker\n";
+std::cout << "5. Rich Joker\n";
+std::cout << "6. Double Joker\n";
+std::cout << "7. Hybrid Joker\n";
 }
